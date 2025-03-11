6d3d407ddf858e5637756fbbbb5b6849656f173d
Sonal Sahwal <sonalsahwal6@gmail.com>
2025-03-11 21:35:34 +0530

    finalize

--- review/urls.py
+++ review/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+    path('submit-url', views.index, name='indsubmit-url'),
+    path('feedback', views.feedback, name='feedback'),
+] 
--- review/views.py
+++ review/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from selenium import webdriver
 import cloudinary
 import cloudinary.uploader
+import requests
+from django.views.decorators.http import require_http_methods
+import json
+from django.http import JsonResponse
+from .models import Review
 
 cloudinary.config( 
     cloud_name = "dsw0zwafj", 
@@ -37,9 +42,93 @@ def take_screenshot(url):
         resource_type='image'
     )
 
-    
+    #print(upload_response['url'])
     return upload_response['url']
 
+def get_review(screenshot_url):
+    url = "https://general-runtime.voiceflow.com/state/user/testuser/interact?logs=off"
+
+    payload = {
+        "action": {
+            "type": "intent",
+            "payload": {
+                "query": screenshot_url,
+                "intent": { "name": "review_intent" },
+                "entities": []
+                        }
+                },
+        "config": {
+            "tts": False,
+            "stripSSML": True,
+            "stopAll": False,
+            "excludeTypes": ["block", "debug", "flow"]
+                },
+        "state": { "variables": { "x_var": 1 } }
+            }
+
+
+    #API key      
+    headers = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "Authorization":"VF.DM.67cf032e57346880627ddf0a.A1eeqnw6wFqbpfIm"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    data = response.json()
+    print("\n\n\nDATA FROM CHATGG\n\n")
+    print(data)
+    review_text= ""
+
+    for item in data:
+        if item['type'] == 'text' and 'payload' in item and 'slate' in item['payload'] and 'content' in item['payload']['slate']:
+            review_text = item['payload']['slate']['content'][0]['children'][0]['text']
+            break
+
+    # print(review_text)
+    return review_text
+
+@require_http_methods(["POST"])
+def submit_url(request):
+    data= json.loads(request.body)
+    domain= data.get('domain')
+
+    website_screenshot=take_screenshot(domain)
+    website_review= get_review(website_screenshot)
+
+    new_review_object= Review.objects.create(
+        site_url= domain,
+        site_image_url= website_screenshot,
+        feedback= website_review,
+    )
+
+    review_id= new_review_object.id
+
+    response_data={
+        'website_screenshot':website_screenshot,
+        'website_review': website_review,
+    }
+
+    return JsonResponse(response_data)
+
+@require_http_methods(["POST"]) 
+def feedback(request):
+    data = json.loads(request.body)
+    review_id = data.get('id')
+    type = data.get('type')
+
+    try:
+        review = Review.objects.get(id=review_id)
+        review.user_rating = type
+        review.save()
+
+        return JsonResponse({"status": "success", "message": "Feedback submitted"})
+    except Review.DoesNotExist:
+        return JsonResponse({"status": "error", "message": "Review not found"})
+
+
 def index(request):
-    take_screenshot("https://felix221123.github.io/my-portfolio-website/")
+    
+    #take_screenshot("https://felix221123.github.io/my-portfolio-website/")
+    #get_review("https://res.cloudinary.com/dsw0zwafj/image/upload/v1741362636/screenshots/___felix221123.github.io_my-portfolio-website_.png.png")
     return render(request, 'index.html')
